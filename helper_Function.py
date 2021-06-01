86da7fc1da9286984c1ecd50d61d3dff775a19bc
PassantElBaroudy <76267023+PassantElBaroudy@users.noreply.github.com>
2021-06-02 01:57:31 +0200

    Update and rename List&StringConversion.py to helper_Function.py

---
+++ helper_Function.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  1 16:06:38 2021
+
+@author: My Lap
+"""
+import math
+
+def Array2String(Array):
+    sep=','
+    strings=[]
+    for element in Array:
+      strings.append(str(element))
+    
+    s=sep.join(strings)
+    return(s)
+
+
+# numbers=Array2String([1,2,3,5,6])
+# print (numbers)
+
+def stringToList(string):
+    p=[]
+    string=string[1:len(string)-1]
+    string=string.replace('  ',' ')
+    listRes = list(string.split(" "))
+    if (listRes.count('')>0):  
+      listRes.remove('')
+    for element in listRes:
+          element_=float(element)
+          p.append(element_)  
+    return p
+
+
+# strA = "[1 2 3 4 5]"
+# print(stringToList(strA))
