c1960497f3b19692320c25f9fb877ac7eb171a0f
PassantElBaroudy <76267023+PassantElBaroudy@users.noreply.github.com>
2021-06-02 16:11:03 +0200

    Update search method.py

--- search method.py
+++ search method.py
@@ -9,24 +9,28 @@ import sqlite3
 import os
 from RGB_CBIR import *
 
-def search (path2,table,method):
+def search (path2,conn,method):
     # TODO: ADD OTHER IF COND. FOR METHODS
-    img_paths=[]
+    c=conn.cursor()
+    c.execute("SELECT * FROM IMG")
+    table=c.fetchall()
+    #img_paths=[]   #law hanrg3 akter men soraa
     image2 = cv2.imread(path2)
     if (method=="RGB_MEAN"):
         vals2 = RGB_MEAN(image2) 
     for i in range(len(table)):
       vals1=stringToList(table[i][2])
       diff = abs(numpy.mean((vals1 - vals2)))
-      if diff <2:
-          img_paths.append(table[i][1])
+      if (diff<1 and diff>0.1):
+          #img_paths.append(table[i][1])  #law hanrg3 akter men sora
+          img_path=table[i][1]
  
-    return img_paths 
+    return img_path 
 
-path2=r'E:\\4th CSE\\2nd Term\\MM\\DataSet/IMG_0595.JPG'
-conn=sqlite3.connect("cbir4.db")
-c=conn.cursor()
-c.execute("SELECT * FROM IMGG")
-table=c.fetchall()
+path2=r'E:\4th CSE\2nd Term\MM\DataSet\IMG_2866.JPG'
+conn=sqlite3.connect("multimedia.db")
+print(search(path2,conn,"RGB_MEAN")) 
 
-print(search(path2,table,"RGB_MEAN")) 
+#c=conn.cursor()
+#c.execute("SELECT * FROM IMGG")
+# table=c.fetchall()
